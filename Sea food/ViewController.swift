d74c626c69c7504bf2ee5701987eaf868e0e3815
Alexei Dudarev <hackeru@shaaaaani.local>
2018-02-21 13:47:00 +0200

    image recognition added to recognise hot dog and burgers

--- Sea food/ViewController.swift
+++ Sea food/ViewController.swift
@@ -29,9 +29,43 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
         
         if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image : userPickedImage) else {
+                fatalError("Could not convert UIImage into CIImage")
+            }
+            detect(image: ciimage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func detect(image : CIImage){
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model Failed.")
+        }
+        let request  = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("Model fail to process image.")
+            }
+            if let firstResult = results.first{
+                if firstResult.identifier.contains("hotdog"){
+                    self.navigationItem.title = "Hotdog!!!"
+                }else if firstResult.identifier.contains("burger"){
+                    self.navigationItem.title = "It's a burger bro!"
+                }else{
+                    self.navigationItem.title = "Not Hotdog!!!"
+                }
+            }
+        }
+        let handler = VNImageRequestHandler(ciImage : image)
+        
+        do{
+            try handler.perform([request])
+            
+        }catch{
+            print (error)
+        }
         
     }
     
